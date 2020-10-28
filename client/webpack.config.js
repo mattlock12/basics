50de498626b180eb5d4700376b480c18411825e6
Matt Leick <matt@mangohealth.com>
2020-10-28 08:26:01 -0700

    create client dir with basic webpack setup
    create api dir with requirements.txt for Django

---
+++ client/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const webpack = require('webpack');
+
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'static/[name][hash].bundle.js'
+  },
+  plugins: [
+    new webpack.ProgressPlugin()
+  ]
+}
